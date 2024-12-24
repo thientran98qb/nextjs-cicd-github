cc6feff95d364484fbc4e91ce7343027ebac6e4e
tms-thientran <thien.tran@tomosia.com>
2024-12-24 10:41:30 +0700

    Fix unitest

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" fetchPriority="high" />
       </Head>
 
       <main>
